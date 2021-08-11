96f0fdd1ec1d97be0cef30e60790191a024b826e
unknown <mrchestalker@gmail.com>
2021-08-11 04:32:14 +0400

    Task 4: nextInt() to nextDouble()

--- src/com/ibs/akalimullin/task4/Average.java
+++ src/com/ibs/akalimullin/task4/Average.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double x = in.nextInt();
-        double y = in.nextInt();
-        double z = in.nextInt();
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        double z = in.nextDouble();
 
         double avg = (x + y + z) / 3;
         System.out.println(avg);
