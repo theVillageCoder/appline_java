254b204518c9d700ce92a0ad4f91c7753c6cfd8d
Костарев Андрей Николаевич (4044254) <akostarev@dev.vtb.ru>
2021-08-24 03:35:17 +0400

    Final task 3 optimized

--- src/com/ibs/akalimullin/finaltask3/UniqueSymbols.java
+++ src/com/ibs/akalimullin/finaltask3/UniqueSymbols.java
@@ -9,26 +9,23 @@ public class UniqueSymbols {
         System.out.print("Количество строк: ");
         int rowsAmount = Integer.parseInt(sc.next());
 
-        String[] strArray = new String[rowsAmount];
-        for (int x = 1; x <= strArray.length; x++) {
-            System.out.printf("Строка %d: ", x);
-            String row = sc.next();
-            strArray[x-1] = row;
-        }
-
         int uniqueCounter = 0;
         String uniqueWord = "";
 
-        for (int i = 0; i < strArray.length; i++) {
-            int uniqueSymbols = strArray[i].replaceAll("(.)(?=.*?\\1)", "").length();
+        for (int x = 1; x <= rowsAmount; x++) {
+            System.out.printf("Строка %d: ", x);
+            String row = sc.next();
+
+            int uniqueSymbols = row.replaceAll("(.)(?=.*?\\1)", "").length();
 
             if (uniqueSymbols > uniqueCounter) {
                 uniqueCounter = uniqueSymbols;
-                uniqueWord = strArray[i];
+                uniqueWord = row;
             } else if (uniqueSymbols == uniqueCounter) {
                 continue;
             }
         }
+
         System.out.println("Ответ: " + uniqueWord);
     }
 }
