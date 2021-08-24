a9701b5a0ea7fec980e25081075ef28fae8b3b36
Костарев Андрей Николаевич (4044254) <akostarev@dev.vtb.ru>
2021-08-24 05:00:19 +0400

    Final task 4 done

---
+++ src/com/ibs/akalimullin/finaltask4/Riddle.java
@@ -0,0 +1,35 @@
+package com.ibs.akalimullin.finaltask4;
+
+import java.util.Scanner;
+
+public class Riddle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Эй, юзер, отгадай загадку! У тебя три попытки!");
+        System.out.println("\"Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает.\"");
+        System.out.println("Если нужна подсказка, просто введи слово \"подсказка\"");
+
+        int i = 0;
+        while (i < 3) {
+            System.out.print("Ответ: ");
+            String answer = sc.nextLine().toLowerCase();
+
+            if (answer.equals("заархивированный вирус") && (i < 3)) {
+                System.out.println("Правильно!");
+                break;
+            } else if (answer.equals("подсказка")) {
+                i += 2;
+                System.out.println("Это вирус, но какой же он?");
+                continue;
+            } else if (i == 0 || i == 1) {
+                System.out.println("Подумай еще!");
+                i++;
+                continue;
+            } else {
+                System.out.println("Обидно, приходи в другой раз");
+                break;
+            }
+        }
+    }
+}
